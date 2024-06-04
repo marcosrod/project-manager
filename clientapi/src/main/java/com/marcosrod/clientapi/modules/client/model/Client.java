f83f3e04fa60dd0d17a5a2cfeb9bed83df2a3927
Marcod18 <marcos-rodrigo05@hotmail.com>
2024-06-04 11:50:57 -0300

    fix: client id sequence generator

--- clientapi/src/main/java/com/marcosrod/clientapi/modules/client/model/Client.java
+++ clientapi/src/main/java/com/marcosrod/clientapi/modules/client/model/Client.java
@@ -13,7 +13,7 @@ public class Client {
 
     @Id
     @SequenceGenerator(name = "client_id_seq", sequenceName = "client_id_seq", allocationSize = 1)
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "client_id_seq")
     private Long id;
 
     @Column(name = "name")
